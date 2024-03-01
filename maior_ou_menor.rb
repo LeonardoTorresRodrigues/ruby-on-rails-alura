9dca36aefe1c42f7e77c2332aa851b99144c3149
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2024-03-01 12:55:07 -0300

    refatoração com for

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -3,27 +3,32 @@ puts "Qual é o seu nome?"
 nome = gets
 
 puts "\n\n\n\n\n\n"
+
 puts "Começaremos o jogo para você, " + nome
 
 puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
 
-puts "\n\n\n\n"
-puts "Tentativa 1"
-puts "Entre com o número"
-chute = gets
-puts "Será que acertou? Você chutou " + chute
+limite_de_tentativas = 5
 
-acertou = numero_secreto == chute.to_i
+for tentativa in 1..limite_de_tentativas
+    puts "\n\n\n\n"
+    puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+    puts "Entre com o número"
+    chute = gets
+    puts "Será que acertou? Você chutou " + chute
 
-if acertou
-    puts "Acertou!"
-else
-    maior = numero_secreto > chute.to_i
-    if maior
-        puts "O número secreto é maior!"
-    else 
-        puts "O número secreto é menor!"
+    acertou = numero_secreto == chute.to_i
+
+    if acertou
+        puts "Acertou!"
+    else
+        maior = numero_secreto > chute.to_i
+        if maior
+            puts "O número secreto é maior!"
+        else 
+            puts "O número secreto é menor!"
+        end
     end
 end
