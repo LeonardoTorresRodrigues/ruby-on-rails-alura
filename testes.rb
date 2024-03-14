9b9229b2b08992ef99f6f9bbde4754415e50b066
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2024-03-14 13:41:17 -0300

    arrays e interpolação de strings

--- testes.rb
+++ testes.rb
@@ -1,14 +1,4 @@
-puts 175 == 175
-puts 175 == 174
-puts "175" == 175
-puts "175" == "175"
-puts "175".to_i == 175 #string to integer
-
-puts
-puts 175 != 174
-puts 175 != 175
-
-puts 175 > 174
-puts 175 >= 175
-puts 175 < 176
-puts 175 <= 175
+nome = gets.strip
+puts "#{nome} tem #{nome.size} caracteres"
+puts "Resultado: "
+puts nome[7]
